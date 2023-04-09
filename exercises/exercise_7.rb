42866fd5d36ba88dd97d3a7e7ae8c378c970d583
Jaewon Bae <jaey.pro@gmail.com>
2023-04-09 00:48:34 -0400

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }, presence: true
+    validates :store_id, presence: true
+end
+
+class Store < ActiveRecord::Base
+    validates :name, length: { minimum: 3 }, presence: true
+    validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }, presence: true
+end
+
+puts 'Create a new store: '
+@store_name = gets.chomp
+@new_store = Store.create(name: @store_name)
+
+puts "Errors? #{Store.create.errors.any?}"
